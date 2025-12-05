d76e5a5882679141ec1c482ed31d3a29a5812aab
Antonio R <antoniorodrigueznoriega@gmail.com>
2025-12-05 18:45:57 +0100

    Added personal keymap for Cantor split keyboard.

--- keyboards/cantor/keymaps/vial/config.h
+++ keyboards/cantor/keymaps/vial/config.h
@@ -2,10 +2,11 @@
 /* keyboard uid */
 #define VIAL_KEYBOARD_UID {0xF0, 0x67, 0x2F, 0x54, 0x23, 0xB3, 0x7B, 0x55}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 3 }
 
 /* default layer count */
-#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
-
+/* fix for issue with keayboard not being detected on PC start */
+#define SPLIT_WATCHDOG_ENABLE
