e6f0258936eb7e6381ffdf41be29895cc125cf4e
Antonio R <antoniorodrigueznoriega@gmail.com>
2026-01-05 10:25:35 +0100

    Change cantor QMK setup for 5x3 hardware option and layout.

--- keyboards/cantor/keymaps/vial/config.h
+++ keyboards/cantor/keymaps/vial/config.h
@@ -2,11 +2,14 @@
 /* keyboard uid */
 #define VIAL_KEYBOARD_UID {0xF0, 0x67, 0x2F, 0x54, 0x23, 0xB3, 0x7B, 0x55}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 1, 2 }
-#define VIAL_UNLOCK_COMBO_COLS { 1, 3 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 5 }
 
 /* default layer count */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
 /* fix for issue with keayboard not being detected on PC start */
 #define SPLIT_WATCHDOG_ENABLE
+
+#define ONESHOT_TAP_TOGGLE 5
+#define ONESHOT_TIMEOUT 5000
